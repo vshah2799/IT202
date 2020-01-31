69225af4dfc73d13bd29a0d9a67b6eff357130ee
Vishal Shah <vshah2799@gmail.com>
2020-01-31 10:09:58 -0500

    Add data to transactions table

--- Week 2/Test.php
+++ Week 2/Test.php
@@ -13,3 +13,10 @@ print "Successfully connected to MySQL.<br><br><br>";
 
 mysqli_select_db( $db, $project );
 
+//define and execute and sql statement
+
+$s = "INSERT INTO TRANSACTIONS VALUES('dave1', '001', '500.00', NOW(), 'N')";
+
+print "<br>SQL insert: $s";
+
+mysqli_query($db, $s) or die(mysqli_error($db));
