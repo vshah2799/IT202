b8a2727272c27b1dd985812e1cdf112da41b19e6
Vishal Shah <vshah2799@gmail.com>
2020-02-06 19:51:29 -0500

    Added $_GET statements

--- Week 2/Test.php
+++ Week 2/Test.php
@@ -16,10 +16,11 @@ print "Successfully connected to MySQL.<br><br><br>";
 mysqli_select_db( $db, $project );
 
 //define and execute and sql statement
-$ucid = "dave1";
-$amount = "500.00";
-$account = '001';
-$s = "INSERT INTO TRANSACTIONS VALUES('$ucid', '$account', '$amount', NOW(), 'N')";
+$ucid = $_GET["ucid"]; print "<br>The ucid is: $ucid";
+$account = $_GET["account"]; print "<br>The account is: $account";
+$amount = $_GET["amount"]; print "<br>The amount is: $amount";
+$mail = $_GET["mail"]; print "<br>The mail is: $mail";
+$s = "INSERT INTO TRANSACTIONS VALUES('$ucid', '$account', '$amount', NOW(), '$mail')";
 print "<br>SQL insert: $s";
 mysqli_query($db, $s) or die(mysqli_error($db));
 
