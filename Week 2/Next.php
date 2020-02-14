6ca5f0a8ee60751397a8c72834d22478cab30342
Vishal Shah <vshah2799@gmail.com>
2020-02-14 09:27:44 -0500

    Added code to Next.php to redirect the user to Form.php if they did not login or tried to directly access this file. It redirects them to Form.php, then Form.php gets them to DatabaseInsertUpdate.php, then they finally get to Next.php

--- Week 2/Next.php
+++ Week 2/Next.php
@@ -1,2 +1,12 @@
 <?php
+
+//Check if the logged entry of $_SESSION is undefined
+if (! isset($_SESSION['logged'])){
+    print("LOGIN PLEASE");
+    header ("refresh: 3 ; url=Form.php");
+    exit();
+}
+
+
+//Testing script code that you shouldn't see unless they have the right credentials
 print("HELLO");
