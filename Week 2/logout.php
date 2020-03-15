4c77254d0883a5ad7166993a70bd3d14a7fa9f3a
vshah <vshah2799@gmail.com>
2020-03-15 01:52:43 -0400

    Added logout.php

---
+++ Week 2/logout.php
@@ -0,0 +1,12 @@
+<?php
+session_start();
+
+$sidvalue = session_id();
+echo "<br>Your session id: " . $sidvalue . "<br>";
+
+$_SESSION = array();		//Make $_SESSION  empty
+session_destroy();			//Terminate session on server
+setcookie("PHPSESSID", "", time()-3600); ;
+
+echo "Your session is terminated.";
+
